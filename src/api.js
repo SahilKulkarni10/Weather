51211c99bc3527beff24d12fe29e943ed28a020f
sahil kulkarni <sahilkulkarni@Sahils-Air.Dlink>
2023-06-14 19:15:31 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '29ce34f8f0mshedcfe5634403e32p14402cjsne1d57748cd74',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "e0cce9c518a05ae2563187727c4a5ca9";
+
