5ea91d2c063170d911ee2bd5cda62d0385163fe9
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-02-25 19:33:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 
+
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 import Forecast from "./components/forecast/forecast";
